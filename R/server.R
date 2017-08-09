b29cdc434c07da1702e82d705b2523cbe7ec5577
Ildiko Czeller <czeildi@gmail.com>
2017-08-09 20:50:58 +0200

    delete unnecessary message

--- R/server.R
+++ R/server.R
@@ -16,8 +16,6 @@ shinypackage_server <- function(input, output) {
     )
 
     getFilteredBirthDt <- function() {
-        message('filtered birth dt function has been called with ', input$period)
-
         cleaned_birth_data %>%
             dplyr::filter(year >= input$period[1] & year <= input$period[2])
     }
